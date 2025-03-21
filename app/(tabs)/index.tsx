c7733c7511dd19d22973b53920911725bf301d7a
gsus-tech <chucho.moralitos0106@gmail.com>
2025-03-21 14:26:18 -0700

    Display the cards in columns

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -62,7 +62,6 @@ export default function HomeScreen() {
     }
   };
   
-
   // Group tasks into sections (todo - in progress - completed)
   const groupTasksByStatus = () => {
     const grouped = {
